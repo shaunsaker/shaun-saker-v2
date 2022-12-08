0b7066a30419a5a298371ed2ca7cb300f8b0e220
Shaun Saker <sakershaun@gmail.com>
2022-12-09 08:11:40 +1000

    Home page background

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Page = styled.div`
   width: 100vw;
   height: 100vh;
+  position: relative;
   display: flex;
   flex-direction: column;
 `
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { Page } from '../../components/page/Page'
+import Background from '../../images/background.png'
 
 interface HomeProps {}
 
 export const Home = ({}: HomeProps): React.ReactElement => {
   return (
     <Page>
-      <Container></Container>
+      <BackgroundImage />
     </Page>
   )
 }
 
-const Container = styled.div``
+const BackgroundImage = styled.div`
+  position: fixed;
+  inset: 0;
+  background-image: url(${Background});
+  background-size: cover;
+  background-position: center center;
+`
