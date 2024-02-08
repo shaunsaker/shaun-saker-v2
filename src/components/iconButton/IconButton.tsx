6dfcb3855092120bd69feec18879dfcc9bedf56a
Shaun Saker <sakershaun@gmail.com>
2024-02-08 16:24:22 +1000

    fix: responsiveness

--- src/components/iconButton/IconButton.tsx
+++ src/components/iconButton/IconButton.tsx
@@ -7,7 +7,7 @@ export const IconButton = ({ className = '', ...props }: Props) => {
   return (
     <button
       className={twMerge(
-        'flex h-8 w-8 items-center justify-center rounded-full border-2 border-white bg-black p-2 text-2xl text-white shadow-lg transition-colors hover:border-teal-400 hover:text-teal-400 focus-visible:outline-teal-400 active:text-teal-400 md:h-16 md:w-16 md:text-3xl',
+        'flex size-16 items-center justify-center rounded-full border-2 border-white bg-black p-2 text-3xl text-white shadow-lg transition-colors hover:border-teal-400 hover:text-teal-400 focus-visible:outline-teal-400 active:text-teal-400',
         className,
       )}
       {...props}
