7ba2b3ca7499a2dead6e507e18e32a9a357b479e
Shaun Saker <sakershaun@gmail.com>
2024-02-09 08:53:32 +1000

    feat: add labels to IconButtons

--- src/components/iconButton/IconButton.tsx
+++ src/components/iconButton/IconButton.tsx
@@ -3,14 +3,18 @@ import { twMerge } from 'tailwind-merge'
 
 type Props = HTMLAttributes<HTMLButtonElement>
 
-export const IconButton = ({ className = '', ...props }: Props) => {
+export const IconButton = ({ className = '', title, children, ...props }: Props) => {
   return (
     <button
       className={twMerge(
-        'flex size-20 items-center justify-center rounded-full border-2 border-white bg-black p-2 text-3xl text-white shadow-lg transition-colors hover:border-teal-500 hover:text-teal-500 focus-visible:outline-teal-400 active:text-teal-400 focus-visible:text-teal-400',
+        'flex flex-col size-20 gap-y-0.5 items-center justify-center rounded-full border-2 border-white bg-black p-2.5 text-3xl text-white shadow-lg transition-colors hover:border-teal-500 hover:text-teal-500 focus-visible:outline-teal-400 active:text-teal-400 focus-visible:text-teal-400',
         className,
       )}
       {...props}
-    ></button>
+    >
+      {children}
+
+      <span className="text-xs font-medium">{title}</span>
+    </button>
   )
 }
