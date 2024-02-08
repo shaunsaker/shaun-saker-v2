e8f408b1e6377e02314e2dfacdcdfaa6a44aa6f0
Shaun Saker <sakershaun@gmail.com>
2024-02-08 19:01:39 +1000

    feat: update IconButton

--- src/components/iconButton/IconButton.tsx
+++ src/components/iconButton/IconButton.tsx
@@ -7,7 +7,7 @@ export const IconButton = ({ className = '', ...props }: Props) => {
   return (
     <button
       className={twMerge(
-        'flex size-16 items-center justify-center rounded-full border-2 border-white bg-black p-2 text-3xl text-white shadow-lg transition-colors hover:border-teal-400 hover:text-teal-400 focus-visible:outline-teal-400 active:text-teal-400',
+        'flex size-20 items-center justify-center rounded-full border-2 border-white bg-black p-2 text-3xl text-white shadow-lg transition-colors hover:border-teal-500 hover:text-teal-500 focus-visible:outline-teal-400 active:text-teal-400 focus-visible:text-teal-400',
         className,
       )}
       {...props}
