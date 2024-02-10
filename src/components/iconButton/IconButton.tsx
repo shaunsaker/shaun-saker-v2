211287bb1cb3c6f5b5589e98312f00900edc0bee
Shaun Saker <sakershaun@gmail.com>
2024-02-10 10:49:11 +1000

    feat: improve Menu responsiveness

--- src/components/iconButton/IconButton.tsx
+++ src/components/iconButton/IconButton.tsx
@@ -7,14 +7,14 @@ export const IconButton = ({ className = '', title, children, ...props }: Props)
   return (
     <button
       className={twMerge(
-        'flex flex-col size-20 gap-y-0.5 items-center justify-center rounded-full border-2 border-white bg-black p-2.5 text-3xl text-white shadow-lg transition-colors hover:border-teal-500 hover:text-teal-500 focus-visible:outline-teal-400 active:text-teal-400 focus-visible:text-teal-400',
+        'flex flex-col size-16 sm:size-20 gap-y-0.5 items-center justify-center rounded-full border-2 border-white bg-black p-2.5 text-3xl text-white shadow-lg transition-colors hover:border-teal-500 hover:text-teal-500 focus-visible:outline-teal-400 active:text-teal-400 focus-visible:text-teal-400',
         className,
       )}
       {...props}
     >
       {children}
 
-      <span className="text-xs font-medium">{title}</span>
+      <span className="hidden sm:block text-xs font-medium">{title}</span>
     </button>
   )
 }
