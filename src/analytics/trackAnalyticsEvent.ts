506f9f61a9710299970edc4f52def6d6577b607d
Shaun Saker <sakershaun@gmail.com>
2024-02-11 08:45:42 +1000

    feat: rename Mixpanel events + turn off debug mode

--- src/analytics/trackAnalyticsEvent.ts
+++ src/analytics/trackAnalyticsEvent.ts
@@ -1,11 +1,11 @@
 import { mixpanel } from '.'
 
-type AnalyticsEvent = 'menuItemClick'
+type AnalyticsEvent = 'Menu Item Click'
 
 type AnalyticsEventData = {
-  menuItemClick: {
+  ['Menu Item Click']: {
     // NOTE: We don't track About and Projects because they are already tracked as page views
-    menuItemId: 'github' | 'linkedin' | 'resume' | 'email'
+    menuItemId: 'Github' | 'LinkedIn' | 'Resume' | 'Email'
   }
 }
 
