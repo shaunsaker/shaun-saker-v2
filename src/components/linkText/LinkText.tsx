1291a526ca5da0dd2acbf676d1bd686b9850d854
Shaun Saker <sakershaun@gmail.com>
2024-02-10 10:49:18 +1000

    feat: update LinkText

--- src/components/linkText/LinkText.tsx
+++ src/components/linkText/LinkText.tsx
@@ -7,7 +7,7 @@ export const LinkText = ({ className = '', ...props }: Props) => {
   return (
     <a
       className={twMerge(
-        'text-teal-400 underline underline-offset-4 transition-colors hover:text-amber-500',
+        'text-teal-400 underline underline-offset-4 transition-colors hover:text-teal-500 focus-visible:outline-teal-400 active:text-teal-400 focus-visible:text-teal-400',
         className,
       )}
       {...props}
